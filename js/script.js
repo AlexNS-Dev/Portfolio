e17c4ebf72f17cd825d607c9dbbc645d65253f1a
Alex Navarro <alex.ns.dev@gmail.com>
2023-07-14 20:24:56 +0200

    about me section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// Personal information
+document.addEventListener('DOMContentLoaded', function() {
+    const birth = new Date(1995, 6, 5);
+    const thisYearsBirthday = new Date(new Date().getFullYear(), 6, 5);
+    const myAgeInMillis = thisYearsBirthday - birth;
+
+    const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25;
+    const myAgeInYears = myAgeInMillis / millisecondsPerYear;
+  
+    if (window.location.href.endsWith('about.html')) {
+      let ageElement = document.getElementsByClassName('age')[0];
+      if (ageElement !== undefined) {
+        ageElement.append(myAgeInYears);
+      }
+    }
+  });
+  
 // Loads a file into an HTML element with a specific ID
 function loadHtml(id, url) {
     fetch(url)
