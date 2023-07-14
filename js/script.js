9de5d6f74215b3c4e0d4cc3943b045b247ac05b4
Alex Navarro <alex.ns.dev@gmail.com>
2023-07-14 20:43:28 +0200

    added navigator languages based on user

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
-// Personal information
-document.addEventListener('DOMContentLoaded', function() {
+// Once page is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    // Personal info
     const birth = new Date(1995, 6, 5);
     const thisYearsBirthday = new Date(new Date().getFullYear(), 6, 5);
     const myAgeInMillis = thisYearsBirthday - birth;
-
     const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25;
     const myAgeInYears = myAgeInMillis / millisecondsPerYear;
-  
+    // Navigator
+    const userLanguage = navigator.language.substring(0, 2);
+
+    // Assigns language
+    document.documentElement.lang = userLanguage;
+
+    // Adds current age dynamically
     if (window.location.href.endsWith('about.html')) {
-      let ageElement = document.getElementsByClassName('age')[0];
-      if (ageElement !== undefined) {
-        ageElement.append(myAgeInYears);
-      }
+        let ageElement = document.getElementsByClassName('age')[0];
+        if (ageElement !== undefined) {
+            ageElement.append(myAgeInYears);
+        }
     }
-  });
-  
+});
+
 // Loads a file into an HTML element with a specific ID
 function loadHtml(id, url) {
     fetch(url)
